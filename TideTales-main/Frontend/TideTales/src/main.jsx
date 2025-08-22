67acb90791d0f34d101332fdcb66dddaa8482987
S Sathprithika <124038347+sathprithika@users.noreply.github.com>
2025-08-22 06:42:54 +0530

    Update main.jsx

--- TideTales-main/Frontend/TideTales/src/main.jsx
+++ TideTales-main/Frontend/TideTales/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Auth0Provider
-    domain="ddev-z2yi34ld63oonr6k.us.auth0.com"
+    domain="dev-z2yi34ld63oonr6k.us.auth0.com"
     clientId="ZhsAy1JTWU49qtt4zZd8SnIG8kXfMOqn"
     authorizationParams={{
       redirect_uri: window.location.origin
